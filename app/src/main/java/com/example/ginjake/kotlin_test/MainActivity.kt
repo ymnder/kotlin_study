3226db1ed536f8b4796a44c5966120ab750a4033
イトウタクヤ <hsginjake123@gmail.com>
2018-01-30 18:39:39 +0900

    ボタン類もviewにした

--- app/src/main/java/com/example/ginjake/kotlin_test/MainActivity.kt
+++ app/src/main/java/com/example/ginjake/kotlin_test/MainActivity.kt
@@ -1,6 +1,5 @@
 package com.example.ginjake.kotlin_test
 
-import android.graphics.Color
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.support.design.widget.NavigationView
@@ -8,22 +7,20 @@ import android.support.v4.view.GravityCompat
 import android.support.v4.widget.DrawerLayout
 import android.util.Log
 import android.view.MenuItem
-import android.widget.Button
-import android.widget.EditText
 import com.example.ginjake.kotlin_test.model.Article
 
 import retrofit2.adapter.rxjava.RxJavaCallAdapterFactory
 
-import android.support.v7.app.ActionBarDrawerToggle
-import android.support.v7.widget.LinearLayoutManager
-import android.support.v7.widget.RecyclerView
+
 import android.support.v7.widget.Toolbar
 import android.support.v7.widget.helper.ItemTouchHelper
+import android.view.Menu
 import android.view.MotionEvent
 import android.view.View
 import android.widget.LinearLayout
 import com.example.ginjake.kotlin_test.client.UpdateClient
 import com.example.ginjake.kotlin_test.view.ArticleListView
+import com.example.ginjake.kotlin_test.view.part.DrawerMenu
 import com.example.ginjake.kotlin_test.view.part.TaskAddButton
 import io.realm.Realm
 import io.realm.RealmConfiguration
@@ -68,34 +65,14 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
         //アップデート確認
         UpdateClient.get();
 
-        /* メニュー*/
-        val toolbar = findViewById<View>(R.id.toolbar) as Toolbar
-        var toggle = object : ActionBarDrawerToggle(
-                this, /* host Activity */
-                drawer, /* DrawerLayout object */
-                toolbar, /* nav drawer icon to replace 'Up' caret */
-                R.string.app_name, /* "open drawer" description */
-                R.string.app_name  /* "close drawer" description */
-        ) {
-            /** Called when a drawer has settled in a completely closed state.  */
-            override fun onDrawerClosed(view: View?) {
-                setTitle("開く")
-            }
-            /** Called when a drawer has settled in a completely open state.  */
-            override fun onDrawerOpened(drawerView: View?) {
-                setTitle("閉じる")
-            }
-        }
+        /* メニュー開閉ボタン*/
+        var Toolbar:DrawerMenu = DrawerMenu()
+        Toolbar.create_menu(this,drawer,
+                findViewById<View>(R.id.toolbar) as Toolbar)
 
-       // drawer.setDrawerListener(toggle)
-        toggle.syncState()
-
-
-
+        /* ナビゲーション */
         val navigationView:NavigationView = findViewById(R.id.navigation_view)
         navigationView.setNavigationItemSelectedListener(this)
-        /* メニューここまで */
-
 
         /* リストビュー*/
         val layout:LinearLayout  = findViewById<View>(R.id.content_main) as LinearLayout
@@ -106,7 +83,6 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
 
     }
 
-    //TODO ビューのidかえる
     override fun onNavigationItemSelected(item: MenuItem): Boolean {
         when (item.itemId) {
             R.id.nav_top -> {
@@ -120,12 +96,13 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
                 article_list.create_list_view(layout)
 
             }
-            R.id.nav_version -> {
+            R.id.nav_delete_all -> {
                 mRealm?.executeTransaction(Realm.Transaction { realm -> realm.deleteAll() })
                 article_list.listAdapter.articles = arrayListOf()
                 article_list.listAdapter.notifyDataSetChanged()
+                drawer.closeDrawers();
             }
-            R.id.nav_gallery -> {
+            R.id.nav_test_data -> {
 
                 //テストデータを追加する
                 article_list.listAdapter.articles.add(
@@ -139,9 +116,9 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
                 )
 
                 article_list.listAdapter.notifyDataSetChanged()
-
+                drawer.closeDrawers();
             }
-            R.id.nav_slideshow -> {
+            R.id.nav_kotori -> {
                 val layout:LinearLayout  = findViewById<View>(R.id.content_main) as LinearLayout// 変更したいレイアウト
                 change_view(layout,R.layout.activity_sub)
             }
